9c38bfb42f556504455b2fada744879b769397d5
Rayyan <122762915+s-rayyan@users.noreply.github.com>
2026-06-04 12:09:55 -0700

    Add 'Yohoho IO' game to the games list

--- public/games.mjs
+++ public/games.mjs
@@ -265,5 +265,6 @@ export const games = [
     { name: 'Webretro', url: 'webretro' },
     { name: 'World\'s Hardest Game', url: 'worldshardestgame' },
     { name: 'World\'s Hardest Game 2', url: 'worldshardestgame2' },
+    { name: 'Yohoho IO', url: 'yohohoio' },
     { name: 'xx142-b2.exe', url: 'xx142b2.exe' }
 ];
